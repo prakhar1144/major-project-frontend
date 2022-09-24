760587da4c404e53f99ad01a0775102e6f362300
prakhar <prakhar841301@gmail.com>
2022-09-24 15:52:28 +0530

    bare minimum homepage created

---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,14 @@
+const Homepage = () => {
+    return (
+        <div>
+            <a href="#">Login</a> <br></br>
+            <a href="#">Register</a> <br></br>
+            <a href="#">Logout</a> <br></br>
+            <a href="#">Forgot Password</a> <br></br>
+            <hr></hr>
+            <a href="#">Nearby Points</a>
+        </div>
+    );
+}
+
+export default Homepage;
