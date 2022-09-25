3f1f81d4ff376d7868d1935daf93caee5388bdc7
prakhar <prakhar841301@gmail.com>
2022-09-25 15:44:14 +0530

    nearby charging station feature added, styling to be added later

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Homepage from './Pages/Homepage'
+import NearbyStation from './Pages/NearbyStation';
 
 function App() {
   return (
     <div>
       {/* Create separate jsx files for all the pages in the website (Homepage, Map Page, Transaction Page) and put in the Pages Folder */}
       <Homepage/>
+      <NearbyStation/>
     </div>
   );
 }
---
+++ src/Pages/NearbyStation.jsx
@@ -0,0 +1,9 @@
+const NearbyStation = () => {
+    return (
+        <>
+            <iframe title="nearby_charging_stations" src="https://www.google.com/maps/d/u/0/embed?mid=1y4_hgDCpK0rKgTD9VFtdoqHV3wol_F4&ehbc=2E312F" width="640" height="480"></iframe>
+        </>
+    );
+}
+
+export default NearbyStation;
