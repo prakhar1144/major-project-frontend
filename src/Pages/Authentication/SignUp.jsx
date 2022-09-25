3c897f6dc1c72bc4afdc5f97ad93f3c874e441c4
prakhar <prakhar841301@gmail.com>
2022-09-25 15:55:37 +0530

    mui default signup checkbox removed

--- src/Pages/Authentication/SignUp.jsx
+++ src/Pages/Authentication/SignUp.jsx
@@ -85,12 +85,6 @@ export default function SignUp() {
                   id="password"
                 />
               </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label={<Typography variant="subtitle2">I want to receive inspiration, marketing promotions and updates via email.</Typography>}
-                />
-              </Grid>
             </Grid>
             <Button
               type="submit"
